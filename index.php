4bbcdc09711a6152ca8578d5b6e646ab054b21dd
SILK Admin <admin@smartinfologiks.com>
2022-05-12 17:12:59 +0530

    Updates for TLS2

--- index.php
+++ index.php
@@ -1,142 +1,124 @@
 <?php
-// Used to check email configurations
+$arrParams = [
+  "smtp_host"=> [
 
-define("ROOT", __DIR__."/");
+  ],
+  "smtp_port"=> [
 
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+  ],
+  "smtp_auth"=> [
 
+  ],
+  "smtp_secure"=> [
 
-$fs=[
-		ROOT."PHPMailer/PHPMailer/phpmailer.inc",
-		ROOT."PHPMailer/PHPMailer/smtp.inc",
-		ROOT."PHPMailer/PHPMailer/pop3.inc",
-	];
-foreach ($fs as $f) {
-	if(file_exists($f)) include_once $f;
-}
+  ],
+  "smtp_username"=> [
 
-$mailConfig = [
-	"smtp_host"=> "smtp.example.com",
-	"smtp_port"=> 587,
-	"smtp_auth"=> true,
-	"smtp_secure"=> "tls",
-	"smtp_username"=> "noreply@example.com",
-	"smtp_password"=> "asd123",
-	"default_from"=> "noreply@example.com",
-	"debug"=> true,
-	"smtp_ssl" => [
-		'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-    ],
+  ],
+  "smtp_password"=> [
+
+  ],
+  "default_from"=> [
+
+  ],
+  "debug"=>[
+
+  ],
+  "to"=> [
+
+  ],
+  "subject"=> [
+
+  ],
+  "body"=> [
+
+  ],
 ];
-$mailMessage = [
-	"to"=>"user1@gmail.com",
-	"subject"=>"MAILTEST Server Email",
-	"body"=>"MAILTEST Server Email for {$mailConfig['smtp_host']}",
-	"attachments"=> false,
-	"cc"=> false,
-	"bcc"=> false,
-];
-
-$notAllowedParams = ["debug"];
-if(!isset($_POST)) {
-	exit("Mail test API works with POST only");
-} else {
-	if(count($_POST)<=0) {
-		echo "<h3>Mail test API works with POST only</h3>";
-		echo "<p>Please find below possible parameters</p>";
-		echo "<ol>";
-		foreach($mailConfig as $a=>$b) {
-			if(is_bool($b)) continue;
-			if(is_array($b)) continue;
-			if(in_array($a, $notAllowedParams)) continue;
-
-			echo "<li>{$a}</li>";
-		}
-		foreach($mailMessage as $a=>$b) {
-			if(is_bool($b)) continue;
-			if(is_array($b)) continue;
-			if(in_array($a, $notAllowedParams)) continue;
-
-			echo "<li>{$a}</li>";
-		}
-		echo "</ol>";
-		exit();
-	}
-}
-//Consume input POST params
-foreach($mailConfig as $a=>$b) {
-	if(is_bool($b)) continue;
-	if(is_array($b)) continue;
-	if(in_array($a, $notAllowedParams)) continue;
-
-	if(isset($_POST[$a])) {
-		$mailConfig[$a] = $_POST[$a];
-	}
-}
-foreach($mailMessage as $a=>$b) {
-	if(is_bool($b)) continue;
-	if(is_array($b)) continue;
-	if(in_array($a, $notAllowedParams)) continue;
-
-	if(isset($_POST[$a])) {
-		$mailMessage[$a] = $_POST[$a];
-	}
-}
-
-//Check if debug information printing is required
-if(isset($_GET['debug']) && $_GET['debug']=="true") {
-	echo "<pre>";
-	print_r($mailConfig);
-	print_r($mailMessage);
-	echo "</pre>";
-	exit();
-}
-
-$mail = new PHPMailer();
-$mail->isSMTP();
-
-// Enable verbose debug output
-$mail->SMTPDebug = 3;
-$mail->Debugoutput = 'html';
-
-$mail->CharSet = 'UTF-8';
-$mail->SMTPOptions = [
-	'ssl' => $mailConfig['smtp_ssl']
-];
-
-//Set the hostname of the mail server
-$mail->Host = $mailConfig['smtp_host'];
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = $mailConfig['smtp_port'];
-//Set the encryption system to use - ssl (deprecated) or tls
-$mail->SMTPSecure = $mailConfig['smtp_secure'];
-//Whether to use SMTP authentication
-$mail->SMTPAuth = $mailConfig['smtp_auth'];
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = $mailConfig['smtp_username'];
-//Password to use for SMTP authentication
-$mail->Password = $mailConfig['smtp_password'];
-
-$mail->setFrom($mailConfig['default_from']);
-
-
-$mail->addAddress($mailMessage['to']);
-$mail->Subject = $mailMessage['subject'];
-$mail->msgHTML($mailMessage['body']);
-$mail->AltBody = strip_tags($mailMessage['body']);
-
-if($mailMessage['cc']) $mail->addCC($mailMessage['cc']);
-if($mailMessage['bcc']) $mail->addBCC($mailMessage['bcc']);
-if($mailMessage['attachments']) $mail->addAttachment($mailMessage['attachments']);
-
-echo "Sending Test mail to : <b>{$mailMessage['to']}</b> with subject <b>{$mailMessage['subject']}</b><br><br>";
-
-if (!$mail->send()) {
-	echo "<pre>";
-	var_dump(array("error"=>true,"msg"=>$mail->ErrorInfo,"type"=>"smtp"));
-} else {
-	echo "Success";
-}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Mailtest Form</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<style>
+h2 {
+  text-align: center;
+  margin-bottom: 25px;
+}
+.form-group {
+  height: 25px;
+}
+label {
+  line-height: 34px;
+}
+</style>
+</head>
+<body>
+
+<div class="container">
+  <h2>MAIL Testing Form</h2>
+  <form action="./push.php" method='POST' enctype="multipart/form">
+    <?php
+      foreach($arrParams as $key=>$conf) {
+        $title = strtoupper(str_replace("_", " ", $key));
+        $inputField = "<input type='text' class='form-control' name='{$key}' placeholder='ENTER {$title}' required>";
+
+        switch($key) {
+          case "subject":
+            $inputField = "<input type='text' class='form-control' name='{$key}' placeholder='ENTER {$title}' required value='Test Email'>";
+          break;
+          case "body":
+            $inputField = "<textarea class='form-control' name='{$key}' placeholder='ENTER {$title}'></textarea>";
+          break;
+          case "smtp_port":
+            $inputField = "<input type='text' class='form-control' name='{$key}' placeholder='ENTER {$title}' list='portList' value='587' required>";
+            $inputField.="<datalist id='portList'>";
+            $inputField.="<option value='587'>";
+            $inputField.="<option value='25'>";
+            $inputField.="<option value='465'>";
+            $inputField.="<option value='110'>";
+            $inputField.="</datalist>";
+          break;
+          case "smtp_auth":
+            $inputField = "<select class='form-control select' name='{$key}'>".
+                "<option value='true'>True</option>".
+                "<option value='false'>False</option>".
+                "</select>";
+          break;
+          case "smtp_secure":
+            $inputField = "<select class='form-control select' name='{$key}'>".
+                "<option value='tls'>tls</option>".
+                "<option value='tls2'>tls2</option>".
+                "<option value='ssl'>ssl</option>".
+                "<option value=''>none</option>".
+                "</select>";
+          break;
+          case "debug":
+            $inputField = "<select class='form-control select' name='{$key}'>".
+                "<option value='false'>False</option>".
+                "<option value='true'>True</option>".
+                "</select>";
+          break;
+        }
+      ?>
+      <div class="form-group">
+        <label class="control-label col-sm-2" for="email"><?=$title?>:</label>
+        <div class="col-sm-10"><?=$inputField?></div>
+      </div>
+      <?php
+      }
+    ?>
+    <div class="form-group" style='margin-top: 50px;'>
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
